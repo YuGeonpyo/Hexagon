b9506573d36c2b244b3ea5c32bfd783d795d93c2
hexagon <hexagon@DESKTOP-8HU6DB7>
2023-11-30 17:02:04 +0900

    구구단

---
+++ YuGeonpyo/src/ygp/banbok.java
@@ -0,0 +1,20 @@
+package ygp;
+
+
+
+public class banbok {
+
+	public static void main(String[] args) {
+		   // while 또는 for문을 사용하여
+		  // 구구단 출력
+		
+		for(int i = 1; i < 10; i++) {
+			for(int j = 1; j < 10; j++) {
+				
+			   System.out.printf("%d * %d = %d\n", i,j,i*j);
+			}
+		
+		}
+	}
+
+}
