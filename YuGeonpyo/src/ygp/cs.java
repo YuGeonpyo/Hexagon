584909b10a354153d258f7a98ad890a889063aa1
hexagon <hexagon@DESKTOP-8HU6DB7>
2023-11-30 17:41:49 +0900

    공부할 거

---
+++ YuGeonpyo/src/ygp/cs.java
@@ -0,0 +1,91 @@
+/*package ygp;
+
+public class cs {
+
+}
+UseVariable
+대소문자 구분 : 카멜 타입 --> java 계열
+
+Use_Variable
+언더바 구분 : 스네이크 타입 --> c언어 계열
+
+●변수
+- data를 담는 그릇
+  1. data 종류 : 숫자, 문자열, 문자, 논리(참, 거짓) 
+    1.1 문자 : char('A', 'a', '가' 등 한 글자)---> 2byte
+
+    1.2 숫자 
+      int(정수, 1, 20, 40, 125...) --> 4byte
+      float(실수, 0.1, 1.0, 1.25...) --> 4byte 잘 안쓴다잉
+      double(실수, 0.1, 1.0, 1.25...) --> 8byte
+
+
+    1.3 문자열 : String('안녕하세요 여러분' 등)
+
+    1.4 논리 : boolean(true 1, false 0) --> 1byte
+
+
+
+●연산자
+- 사칙연산
+ 1. +
+ 2. -
+ 3. /
+ 4. *
+ 5. %(mod) - 나머지 연산자
+ 6. = : 대입
+ 7. == : 같다
+  8. && : and
+ 9. || : or
+10. != : not
+
+
+●코드의 흐름 제어
+
+1. if문
+    if([조건]){
+                    조건이 true일 때 실행 될 내용
+}
+
+ex) if(1 < 2) { 
+           system.out.println("1이 2보다 작다");
+
+
+
+
+2. switch 문
+     switch(조건){
+     case 1 : 
+
+     case 2 :
+
+   case 3 :
+
+   ...
+   default: // case에 해당하는 조건이 없으면
+
+
+● 반복문
+- while, for
+
+1. while문
+    while([조건]){
+                 //조건이 참일 때 실행
+
+}
+ex) num = 0;
+    while(num < 100){
+         num += 1;
+         //또는 num++
+
+
+
+2. for문
+     for([조건]){
+               //조건이 참일 때  실행
+
+}
+
+ex) for(int i = 0; i < 100 ; i++){
+        //참일 경우 실행
+*/
