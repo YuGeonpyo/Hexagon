dfaa16bfe3193bff63eea108f178ce267d373839
hexagon <hexagon@DESKTOP-8HU6DB7>
2023-11-30 16:58:47 +0900

    별 트리 만들기

---
+++ YuGeonpyo/src/ygp/tree.java
@@ -0,0 +1,20 @@
+package ygp;
+
+public class tree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+             for (int i = 1; i< 10; i+=2) {
+            	  for(int j = 9; j>i; j-=2){
+            		  System.out.print(" ");
+            	  }
+            	  for(int j = 0; j < i; j++) {
+            		  System.out.print("*");
+            	  }
+            	  System.out.println();
+            	  
+            	  }
+             }
+	}
+
+
